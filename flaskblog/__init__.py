06a526a5c0420cc00b2d6435019993a060b717ef
Keng Hwee <kenghwee.t@gmail.com>
2019-06-17 01:39:44 +0800

    Completed blueprints section

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,32 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '278b409266a83b9b013cba8706922a81'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = "users.login"  # to tell the app where the login app is located
+login_manager.login_message_category = "info"
+mail = Mail()
 
-from flaskblog import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flaskblog import app  # import for init file of flaskblog package
+from flaskblog import create_app  # import for init file of flaskblog package
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
